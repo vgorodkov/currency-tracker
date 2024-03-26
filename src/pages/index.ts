c85c92b03a83a8559190e001f887f34d3fed0e87
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-26 20:31:50 +0300

    chore: pages reexports

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { LazyBankCard } from './bankcard/BankCard.lazy';
+export { LazyContacts } from './contacts/Contacts.lazy';
+export { LazyHome } from './home/Home.lazy';
+export { LazyTimeline } from './timeline/Timeline.lazy';
