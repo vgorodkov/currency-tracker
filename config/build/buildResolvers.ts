4945158e4a32a012f578bf522ceead8ae35431c7
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-26 20:22:36 +0300

    chore: add aliases

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -6,5 +6,8 @@ export const buildResolvers = (
 ): Configuration['resolve'] => {
   return {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': options.paths.src,
+    },
   };
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,16 +3,17 @@ import webpack from 'webpack';
 import { buildWebpack } from './config/build/buildWebpack';
 import { BuildMode, BuildPaths } from './config/build/types';
 
-type envVariables = {
+type EnvVariables = {
   mode: BuildMode;
   port: number;
 };
 
-export default (env: envVariables) => {
+export default (env: EnvVariables) => {
   const paths: BuildPaths = {
     output: resolve(__dirname, 'build'),
     html: resolve(__dirname, 'public', 'index.html'),
     entry: resolve(__dirname, 'src', 'index.tsx'),
+    src: resolve(__dirname, 'src'),
   };
   const config: webpack.Configuration = buildWebpack({
     port: env.port ?? 3000,
