27c9370d8568f00319b94823af59d60e1bcb6d38
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-27 20:17:20 +0300

    refactor: decompose header to separate components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,10 @@
-import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
-
-const ROUTES = [
-  { path: '/', name: 'Home' },
-  { path: '/timeline', name: 'Timeline' },
-  { path: '/bankcard', name: 'Bank card' },
-  { path: '/contacts', name: 'Contacts' },
-];
+import { HeaderNav } from '@/components/Header/components/HeaderNav';
 
 export function Header() {
   return (
-    <nav className={styles.topnav}>
-      {ROUTES.map((item) => (
-        <Link key={item.name} className={styles.navElement} to={item.path}>
-          {item.name}
-        </Link>
-      ))}
-    </nav>
+    <header className={styles.header}>
+      <HeaderNav />
+    </header>
   );
 }
