1ac892b40041982f6f2515ab8f45a928861c751d
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-27 22:03:17 +0300

    feat: header hero component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import styles from './styles.module.scss';
+import { HeaderHero } from '@/components/Header/components/HeaderHero';
 import { HeaderNav } from '@/components/Header/components/HeaderNav';
 
 export function Header() {
   return (
     <header className={styles.header}>
       <HeaderNav />
+      <HeaderHero />
     </header>
   );
 }
