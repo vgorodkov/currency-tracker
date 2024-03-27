f4153faf27c49d5fdd118d87932384fbdea2810c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-27 15:40:13 +0300

    feat: implement toggle theme button

---
+++ src/components/ToggleThemeBtn/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import styles from './styles.module.scss';
+
+export function ToggleThemeBtn() {
+  return (
+    <>
+      <input className={styles.input} type="checkbox" id="darkmode-toggle" />
+      <label className={styles.label} htmlFor="darkmode-toggle" />
+    </>
+  );
+}
