3f19cdea3ff94f6d94c99a3d7ddf6ae1456b1827
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-28 13:09:00 +0300

    feat: add currency cards to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,18 @@
+import CurrencyCard from '@/components/CurrencyCard';
 import styles from './styles.module.scss';
+
+const MOCK_DATA = Array(11)
+  .fill('')
+  .map((_, index) => index);
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Home</h1>
+      <div className={styles.currencyList}>
+        {MOCK_DATA.map((item) => (
+          <CurrencyCard key={item} />
+        ))}
+      </div>
     </div>
   );
 }
