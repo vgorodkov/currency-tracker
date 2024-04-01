4c05f9193e6c7c2f2ab70b3b077e27f6e62a583e
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-01 20:57:20 +0300

    chore: util function to map currencies with existing imgs

---
+++ src/utils/mapIcons.ts
@@ -0,0 +1,19 @@
+import audSvg from '@/assets/icons/australian_dollar.svg?url';
+import btcSvg from '@/assets/icons/bitcoin.svg?url';
+import cadSvg from '@/assets/icons/canadian_dollar.svg?url';
+import eurSvg from '@/assets/icons/euro.svg?url';
+import gbpSvg from '@/assets/icons/libra.svg?url';
+import arsSvg from '@/assets/icons/peso_argentino.svg?url';
+import cnySvg from '@/assets/icons/won.svg?url';
+import jpySvg from '@/assets/icons/yen.svg?url';
+
+export const icons: Record<string, string> = {
+  BTC: btcSvg,
+  AUD: audSvg,
+  ARS: arsSvg,
+  EUR: eurSvg,
+  CAD: cadSvg,
+  JPY: jpySvg,
+  CNY: cnySvg,
+  GBP: gbpSvg,
+};
