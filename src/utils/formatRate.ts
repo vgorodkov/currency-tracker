9ca34d2efc5508746e299479b0ec4a481cbc6f9a
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-01 20:56:30 +0300

    chore: set default toFlip to false because use invert param as default in api

--- src/utils/formatRate.ts
+++ src/utils/formatRate.ts
@@ -1,4 +1,4 @@
-export const formatRate = (rate: number, toFlip: boolean = true) => {
+export const formatRate = (rate: number, toFlip: boolean = false) => {
   const value = toFlip ? 1 / rate : rate;
 
   if (Math.abs(value) < 0.01) {
