6d88b178282e9ab6130ffe27624c89c84bae5414
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-01 21:05:14 +0300

    feat: create theme context

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from '@/App';
+import { ThemeProvider } from '@/context/ThemeContext';
 
 const rootElement = document.getElementById('root');
 
@@ -17,7 +18,9 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
