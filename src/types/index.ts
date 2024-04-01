81bf75902e9cfbb63cfa3e6b9088d0500f37e273
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-01 20:56:56 +0300

    chore: add types for new endpoint

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export interface CurrencyInfo {
   rate: number;
-  asset_id_quote: string;
+  asset_id: string;
 }
 
 export interface ExchangeRate extends CurrencyInfo {
@@ -11,4 +11,27 @@ export interface ConverterCurrency {
   fromCurrency: string;
   toCurrency: string;
   rate: number;
+}
+
+export interface ExchangeAsset {
+  asset_id?: string;
+  name?: string;
+  type_is_crypto: number;
+  data_quote_start?: Date;
+  data_quote_end?: Date;
+  data_orderbook_start?: Date;
+  data_orderbook_end?: Date;
+  data_trade_start?: Date;
+  data_trade_end?: Date;
+  data_symbols_count?: number;
+  volume_1hrs_usd?: number;
+  volume_1day_usd?: number;
+  volume_1mth_usd?: number;
+  price_usd?: number;
+  id_icon?: string;
+  supply_current?: number;
+  supply_total?: number;
+  supply_max?: number;
+  data_start?: string;
+  data_end?: string;
 }
