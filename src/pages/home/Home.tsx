162e73973342808e840dc9a9b0707f136ae55808
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-02 16:52:42 +0300

    refactor: move logic to rtk slices

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,29 +1,11 @@
-import { useCallback, useState } from 'react';
-
 import { ConverterModal } from '@/components/ConverterModal';
 import { CurrencyExchangeList } from '@/components/CurrencyExchangeList';
-import { Modal } from '@/components/Modal';
-import { CurrencyInfo } from '@/types';
 
 const Home = () => {
-  const [isModalActive, setIsModalActive] = useState(false);
-  const [fromCurrencyInfo, setFromCurrencyInfo] = useState<CurrencyInfo | null>(null);
-
-  const closeModal = () => {
-    setIsModalActive(false);
-  };
-
-  const onCurrencyCardClick = useCallback((currencyInfo: CurrencyInfo) => {
-    setFromCurrencyInfo(currencyInfo);
-    setIsModalActive(true);
-  }, []);
-
   return (
     <main>
-      <Modal isActive={isModalActive} closeModal={closeModal}>
-        {fromCurrencyInfo && <ConverterModal fromCurrencyInfo={fromCurrencyInfo} />}
-      </Modal>
-      <CurrencyExchangeList onCurrencyCardClick={onCurrencyCardClick} />
+      <ConverterModal />
+      <CurrencyExchangeList />
     </main>
   );
 };
