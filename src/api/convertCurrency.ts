47820444112ae5d2da42438cde12342a950c40c5
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-02 16:56:43 +0300

    refactor: remove not used function

--- src/api/convertCurrency.ts
+++ src/api/convertCurrency.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-import { withCache } from '@/utils/withCache';
-
 const API_URL = 'https://rest.coinapi.io/v1/exchangerate';
 
-const getConvertedCurrency = async (from: string, to: string) => {
+export const getConvertedCurrency = async (from: string, to: string) => {
   try {
     const response = await axios.get(`${API_URL}/${from}/${to}`, {
       headers: {
@@ -17,9 +15,4 @@ const getConvertedCurrency = async (from: string, to: string) => {
   } catch (e) {
     throw new Error('Erorr while converting currency');
   }
-};
-
-export const convertCurrency = async (from: string, to: string) => {
-  const data = await withCache(() => getConvertedCurrency(from, to), `${from}-${to}`);
-  return data;
 };
