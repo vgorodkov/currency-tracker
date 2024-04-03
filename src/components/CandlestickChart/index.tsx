c509536e653c82ef210ceaba3cd4ed72c256d31f
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-03 19:33:55 +0300

    feat: create basic candlestick chart

---
+++ src/components/CandlestickChart/index.tsx
@@ -0,0 +1,129 @@
+import Chart from 'chart.js/auto';
+import { Component, createRef, RefObject } from 'react';
+
+import { CandlestickData } from '@/types';
+
+import { candlestickPlugin } from './plugins/candlestickPlugin';
+import { crosshairPlugin } from './plugins/crosshairPlugin';
+
+interface CandlestickChartProps {
+  candleSticksData: CandlestickData[];
+}
+
+class CandlestickChart extends Component<CandlestickChartProps> {
+  private canvasRef: RefObject<HTMLCanvasElement>;
+
+  private chartRef: Chart | null;
+
+  constructor(props: CandlestickChartProps) {
+    super(props);
+    this.canvasRef = createRef<HTMLCanvasElement>();
+    this.chartRef = null;
+  }
+
+  componentDidMount() {
+    this.renderChart();
+  }
+
+  componentDidUpdate() {
+    this.renderChart();
+  }
+
+  componentWillUnmount() {
+    if (this.chartRef) {
+      this.chartRef.destroy();
+    }
+  }
+
+  renderChart() {
+    const { candleSticksData } = this.props;
+
+    if (this.chartRef) {
+      this.chartRef.destroy();
+    }
+
+    const canvasCtx = this.canvasRef.current?.getContext('2d');
+    if (canvasCtx) {
+      this.chartRef = new Chart(canvasCtx, {
+        type: 'bar',
+        data: {
+          datasets: [
+            {
+              data: candleSticksData,
+              backgroundColor: (ctx) => {
+                const {
+                  raw: { o, c },
+                } = ctx;
+
+                let color;
+                if (c >= o) {
+                  color = '#16C782';
+                } else {
+                  color = '#EA3943';
+                }
+                return color;
+              },
+              borderColor: '#FF971D',
+              borderWidth: 2,
+              borderSkipped: false,
+            },
+          ],
+        },
+        options: {
+          layout: {
+            padding: {
+              left: 24,
+              bottom: 24,
+            },
+          },
+          parsing: {
+            xAxisKey: 'x',
+            yAxisKey: 's',
+          },
+          scales: {
+            x: {
+              type: 'timeseries',
+              time: {
+                unit: 'day',
+                tooltipFormat: 'MMM, d, yyyy',
+              },
+            },
+            y: {
+              beginAtZero: true,
+              grace: 1,
+            },
+          },
+          plugins: {
+            legend: {
+              display: false,
+            },
+            tooltip: {
+              callbacks: {
+                beforeBody: (ctx) => {
+                  const { o, h, l, c } = ctx[0].raw;
+                  const bodyArr = [`O: ${o}`, `H: ${h}`, `L: ${l}`, `C: ${c}`];
+                  return bodyArr;
+                },
+                label: () => {
+                  return '';
+                },
+              },
+            },
+          },
+        },
+        plugins: [candlestickPlugin, crosshairPlugin],
+      });
+    }
+  }
+
+  render() {
+    const { candleSticksData } = this.props;
+    if (candleSticksData.length < 1) {
+      return <h1 style={{ color: 'wheat' }}>Please fill data</h1>;
+    }
+
+    return <canvas ref={this.canvasRef} />;
+  }
+}
+
+export default CandlestickChart;
