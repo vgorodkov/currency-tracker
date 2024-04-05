22573f5fd7d30564651d57582bfc58b8babe0b65
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-05 14:01:39 +0300

    chore: map icons and name to currency code

--- src/constants/currencies.ts
+++ src/constants/currencies.ts
@@ -1,1 +1,45 @@
+import audSvg from '@/assets/icons/australian_dollar.svg?url';
+import btcSvg from '@/assets/icons/bitcoin.svg?url';
+import cadSvg from '@/assets/icons/canadian_dollar.svg?url';
+import eurSvg from '@/assets/icons/euro.svg?url';
+import gbpSvg from '@/assets/icons/libra.svg?url';
+import arsSvg from '@/assets/icons/peso_argentino.svg?url';
+import cnySvg from '@/assets/icons/won.svg?url';
+import jpySvg from '@/assets/icons/yen.svg?url';
+
 export const currencies = ['BTC', 'ARS', 'EUR', 'CAD', 'JPY', 'AUD', 'CNY', 'GBP'];
+
+export const currenciesInfo: Record<string, { img: string; name: string }> = {
+  BTC: {
+    img: btcSvg,
+    name: 'Bitcoin',
+  },
+  ARS: {
+    img: arsSvg,
+    name: 'Argentine Peso',
+  },
+  EUR: {
+    img: eurSvg,
+    name: 'Euro',
+  },
+  CAD: {
+    img: cadSvg,
+    name: 'Canadian Dollar',
+  },
+  JPY: {
+    img: jpySvg,
+    name: 'Yen',
+  },
+  AUD: {
+    img: audSvg,
+    name: 'Australian dolar',
+  },
+  CNY: {
+    img: cnySvg,
+    name: 'Yuan',
+  },
+  GBP: {
+    img: gbpSvg,
+    name: 'British pounds',
+  },
+};
