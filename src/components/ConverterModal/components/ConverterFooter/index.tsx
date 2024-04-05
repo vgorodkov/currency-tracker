d2b2abdb8b8a72b4931e2e22302aae3850aa19d6
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-05 17:10:47 +0300

    feat: create new converter footer

---
+++ src/components/ConverterModal/components/ConverterFooter/index.tsx
@@ -0,0 +1,40 @@
+import { useSelector } from 'react-redux';
+
+import { Button } from '@/components/Button';
+import { Dropdown } from '@/components/Dropdown';
+import { currencies } from '@/constants/currencies';
+import { convertCurrency, setToCurrency } from '@/redux/slices/converterSlice';
+import { RootState, useAppDispatch } from '@/redux/store';
+
+import styles from './styles.module.scss';
+
+export const ConverterFooter = () => {
+  const dispatch = useAppDispatch();
+
+  const toCurrencyCode = useSelector((state: RootState) => state.converter.toCurrency);
+  const fromCurrencyCode = useSelector((state: RootState) => state.converter.fromCurrency.code);
+  const isLoading = useSelector((state: RootState) => state.converter.isLoading);
+
+  const isBtnDisabled = !toCurrencyCode || isLoading;
+  const avaibleCurrencies = currencies.filter((currency) => currency !== fromCurrencyCode);
+
+  const onConverterBtnClick = () => {
+    dispatch(convertCurrency());
+  };
+
+  const selectToCurrency = (toCurrency: string) => {
+    dispatch(setToCurrency(toCurrency));
+  };
+
+  return (
+    <div className={styles.converterFooter}>
+      <Button title="Convert" onClick={onConverterBtnClick} disabled={isBtnDisabled} />
+      <Dropdown
+        options={avaibleCurrencies}
+        selected={toCurrencyCode || 'USD'}
+        handleSelect={selectToCurrency}
+        pos="above"
+      />
+    </div>
+  );
+};
--- src/components/ConverterModal/components/index.js
+++
@@ -1,3 +0,0 @@
-export { ConverterButton } from './ConverterButton';
-export { ConverterList } from './ConverterList';
-export { ConverterSelection } from './ConverterSelection';
