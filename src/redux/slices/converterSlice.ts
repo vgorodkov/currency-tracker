25adc03dd39f04d6a277d8ba1e87c69c1e286cd6
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-07 22:14:35 +0300

    chore: change logic to store only onve converted value instead of list

--- src/redux/slices/converterSlice.ts
+++ src/redux/slices/converterSlice.ts
@@ -5,12 +5,10 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 
 import { getConvertedCurrency } from '@/api/convertCurrency';
-import { BASE_CURRENCY } from '@/constants/currencies';
-import { ConverterCurrency } from '@/types';
 
 import { createAppAsyncThunk } from '../helpers/createAppAsyncThunk';
 
-interface FromCurrency {
+interface Currency {
   code: string;
   rate: number;
 }
@@ -19,48 +17,34 @@ export const convertCurrency = createAppAsyncThunk(
   'converter/convertCurrency',
   async (_, { rejectWithValue, getState, dispatch }) => {
     try {
-      const to = getState().converter.toCurrency;
+      const to = getState().converter.toCurrency.code;
       const from = getState().converter.fromCurrency.code;
       const response = await getConvertedCurrency(from, to);
       const { rate } = response.data;
-      dispatch(setConvertedCurrencies({ fromCurrency: from, toCurrency: to, rate }));
+      dispatch(setToCurrencyRate(rate));
       return response.data;
     } catch (err) {
       return rejectWithValue(err.message);
     }
-  },
-  {
-    condition: (_, { getState }) => {
-      const state = getState();
-      const to = getState().converter.toCurrency;
-      const from = getState().converter.fromCurrency.code;
-      const isConverted = state.converter.convertedCurrencies.some(
-        (converted) => converted.fromCurrency === from && converted.toCurrency === to
-      );
-
-      if (isConverted) {
-        return false;
-      }
-      return true;
-    },
   }
 );
 
 export interface ConverterState {
   isConverterOpen: boolean;
-  toCurrency: string | null;
-  fromCurrency: FromCurrency | null;
+  toCurrency: Currency;
+  fromCurrency: Currency | null;
   fromRate: number;
-  convertedCurrencies: ConverterCurrency[];
   isLoading: boolean;
 }
 
 const initialState: ConverterState = {
   isConverterOpen: false,
-  toCurrency: null,
+  toCurrency: {
+    code: '',
+    rate: -1,
+  },
   fromCurrency: null,
   fromRate: 0,
-  convertedCurrencies: [],
   isLoading: false,
 };
 
@@ -69,30 +53,20 @@ export const converterSlice = createSlice({
   initialState,
   reducers: {
     openConverter: (state) => {
-      state.toCurrency = BASE_CURRENCY;
       state.isConverterOpen = true;
     },
     closeConverter: (state) => {
       state.isConverterOpen = false;
     },
-    setFromCurrency: (state, action: PayloadAction<FromCurrency>) => {
+    setFromCurrency: (state, action: PayloadAction<Currency>) => {
       const { code, rate } = action.payload;
       state.fromCurrency = { code, rate };
     },
-    setToCurrency: (state, action) => {
-      state.toCurrency = action.payload;
+    setToCurrencyCode: (state, action) => {
+      state.toCurrency.code = action.payload;
     },
-    setConvertedCurrencies: (state, action: PayloadAction<ConverterCurrency>) => {
-      const isAlreadyInList = state.convertedCurrencies.some(
-        (currency) =>
-          currency.fromCurrency === action.payload.fromCurrency &&
-          currency.toCurrency === action.payload.toCurrency
-      );
-
-      if (isAlreadyInList) {
-        return;
-      }
-      state.convertedCurrencies.push(action.payload);
+    setToCurrencyRate: (state, action) => {
+      state.toCurrency.rate = action.payload;
     },
   },
   extraReducers(builder) {
@@ -110,7 +84,7 @@ export const {
   openConverter,
   closeConverter,
   setFromCurrency,
-  setConvertedCurrencies,
-  setToCurrency,
+  setToCurrencyCode,
+  setToCurrencyRate,
 } = converterSlice.actions;
 export default converterSlice.reducer;
