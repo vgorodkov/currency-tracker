37df194d2b3130cc5f4465cb632305e6165c937b
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-07 22:17:11 +0300

    chore: add dummy value if the price unknown

--- src/utils/formatRate.ts
+++ src/utils/formatRate.ts
@@ -1,4 +1,7 @@
 export const formatRate = (rate: number, toFlip: boolean = false) => {
+  if (rate < 0) {
+    return '???';
+  }
   const value = toFlip ? 1 / rate : rate;
 
   if (Math.abs(value) < 0.01) {
