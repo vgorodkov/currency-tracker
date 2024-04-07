bcc4705f98d7ff2b9eeccd5271bdb7116804d94e
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-07 22:09:04 +0300

    refactor: change converter modal to store only base information and one converted

--- src/components/ConverterModal/index.tsx
+++ src/components/ConverterModal/index.tsx
@@ -4,12 +4,13 @@ import { Modal } from '@/components/Modal';
 import { closeConverter } from '@/redux/slices/converterSlice';
 import { RootState, useAppDispatch } from '@/redux/store';
 
+import { ConverterCurrencies } from './components/ConverterCurrencies';
 import { ConverterFooter } from './components/ConverterFooter';
-import { ConverterList } from './components/ConverterList';
 import styles from './styles.module.scss';
 
 export const ConverterModal = () => {
   const dispatch = useAppDispatch();
+
   const isConverterOpen = useSelector((state: RootState) => state.converter.isConverterOpen);
 
   const closeModal = () => {
@@ -24,7 +25,7 @@ export const ConverterModal = () => {
     <Modal isActive={isConverterOpen} closeModal={closeModal}>
       <div className={styles.converterModal}>
         <h3 className={styles.modalTitle}>Exchange Rates</h3>
-        <ConverterList />
+        <ConverterCurrencies />
         <ConverterFooter />
       </div>
     </Modal>
