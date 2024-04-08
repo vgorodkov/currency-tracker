59bef2bc4b57689a235b0b0391224dcf7603ac42
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-08 12:14:11 +0300

    fix: show at least 8 digits to handle very small numbers like ARS to BTC rate

--- src/utils/formatRate.ts
+++ src/utils/formatRate.ts
@@ -5,7 +5,7 @@ export const formatRate = (rate: number, toFlip: boolean = false) => {
   const value = toFlip ? 1 / rate : rate;
 
   if (Math.abs(value) < 0.01) {
-    return Number(value.toFixed(4));
+    return Number(value.toFixed(8));
   }
 
   return Number(value.toFixed(2));
