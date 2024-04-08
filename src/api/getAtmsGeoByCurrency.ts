62a92bc095d9daab384dc6246ec9dd5c413d4fd7
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-08 20:50:42 +0300

    fix: fix cors error whilte making request to belarusbank api

--- src/api/getAtmsGeoByCurrency.ts
+++ src/api/getAtmsGeoByCurrency.ts
@@ -2,12 +2,9 @@ import axios from 'axios';
 
 import { ApiResponse } from '@/types/api';
 
-const BASE_URL =
-  process.env.mode === 'production' ? 'https://belarusbank.by/open-banking/v1.0/atms' : '/api/atms';
-
 export const getAtmsGeoByCurrency = async (currency: string, limit: number = 50) => {
   try {
-    const res = await axios.get<ApiResponse>(`${BASE_URL}?city=Минск&ATM_currency=${currency}`);
+    const res = await axios.get<ApiResponse>(`/api/atms?city=Минск&ATM_currency=${currency}`);
     const {
       Data: { ATM },
     } = res.data;
