ee1ab675e031f51ab18f4e6a7a1f7ce6aded0367
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-08 12:53:56 +0300

    refactor: rewrite BankCard page to class component

--- src/pages/bankcard/BankCard.tsx
+++ src/pages/bankcard/BankCard.tsx
@@ -1,9 +1,15 @@
-import 'mapbox-gl/dist/mapbox-gl.css';
-
-import { useState } from 'react';
+import { Component } from 'react';
 
 import { ElasticSearch } from '@/components/ElasticSearch';
 import { Map } from '@/components/Map';
+
+import styles from './styles.module.scss';
+
+interface BankCardProps {}
+
+interface BankCardState {
+  currencyQuery: string;
+}
 
 const MINSK_GEO = {
   lng: 27.567444,
@@ -12,34 +18,37 @@ const MINSK_GEO = {
 
 const options = [
   { id: 1, name: 'USD' },
-  {
-    id: 2,
-    name: 'BYN',
-  },
+  { id: 2, name: 'BYN' },
 ];
 
-const BankCard = () => {
-  const [currencyQuery, setCurrencyQuery] = useState(options[0].name);
-  return (
-    <div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginBottom: 32,
-        }}
-      >
-        <ElasticSearch
-          options={options}
-          label="name"
-          selectedVal={currencyQuery}
-          handleChange={(val) => setCurrencyQuery(val)}
-        />
-      </div>
-      <Map startPos={MINSK_GEO} currency={currencyQuery} zoom={10} />
-    </div>
-  );
-};
+class BankCard extends Component<BankCardProps, BankCardState> {
+  constructor(props: BankCardProps) {
+    super(props);
+    this.state = {
+      currencyQuery: options[0].name,
+    };
+  }
+
+  handleCurrencyChange = (val: string) => {
+    this.setState({ currencyQuery: val });
+  };
+
+  render() {
+    const { currencyQuery } = this.state;
+    return (
+      <>
+        <div className={styles.elasticSearchContainer}>
+          <ElasticSearch
+            options={options}
+            label="name"
+            selectedVal={currencyQuery}
+            handleChange={this.handleCurrencyChange}
+          />
+        </div>
+        <Map startPos={MINSK_GEO} currency={currencyQuery} zoom={10} />
+      </>
+    );
+  }
+}
 
 export default BankCard;
