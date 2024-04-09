2e9a598b1a943f9ed02b0b5ed8ec5e565143e595
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-09 17:15:27 +0300

    refactor: move configuration to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { LoadingFallback } from '@/components/LoadingFallback';
 import { useInitTheme } from '@/hooks/useInitTheme';
+import { persistor, store } from '@/redux/store';
 import { AppRouter } from '@/routers/AppRouter';
 
 export const App = () => {
   useInitTheme();
 
-  return <AppRouter />;
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={<LoadingFallback />}>
+          <ErrorBoundary>
+            <AppRouter />
+          </ErrorBoundary>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  );
 };
