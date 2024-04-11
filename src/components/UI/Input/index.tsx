d1ff2ab2b51cb89e54473631d79d3bbc41c74261
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-11 10:32:40 +0300

    fix: remove unnecessary placeholder props because default input has the same

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -2,10 +2,8 @@ import { InputHTMLAttributes } from 'react';
 
 import styles from './styles.module.scss';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  placeholder: string;
-}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input = ({ placeholder, ...props }: InputProps) => {
-  return <input className={styles.inputField} type="text" placeholder={placeholder} {...props} />;
+export const Input = ({ ...props }: InputProps) => {
+  return <input className={styles.inputField} type="text" {...props} />;
 };
