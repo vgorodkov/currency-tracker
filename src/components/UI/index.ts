6bebb70d981e43ae2b49ef521546615000c89526
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-19 11:01:01 +0300

    chore: add reexport

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,5 +1,6 @@
 export { Button } from './Button';
 export { Dropdown } from './Dropdown';
+export { ErrorFallback } from './ErrorFallback';
 export { Input } from './Input';
 export { LoaderSpinner } from './LoaderSpinner';
 export { Modal } from './Modal';
